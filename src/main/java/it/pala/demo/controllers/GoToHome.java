d7622c8d44c1b35f1df7626d66407fba5370b521
MatteoPalazzoli <matteo.palazzoli@mail.polimi.it>
2021-07-04 18:18:35 +0200

    build fix

--- src/main/java/it/pala/demo/controllers/GoToHome.java
+++ src/main/java/it/pala/demo/controllers/GoToHome.java
@@ -2,9 +2,21 @@ package it.pala.demo.controllers;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
-@WebServlet("/GoToHome")
+@WebServlet("/Home")
 public class GoToHome extends HttpServlet {
 
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.getWriter().println("getted");
 
+    }
+
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.getWriter().println("posted");
+    }
 }
